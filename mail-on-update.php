9d05cff914bfe12e958397fb389ea4b7b320b1bb
kubi23 <kubi23@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-03 14:34:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mail-on-update/tags/2.0@115247 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mail-on-update.php
+++ mail-on-update.php
@@ -289,7 +289,6 @@ class MailOnUpdate {
 			</table>
 			<p class="submit"><input type="submit" name="submit" value="<?php echo __('Save', 'mail-on-update'); ?>" /></p>
 		</form>
-		</p>
 		<p>
 		<form action="options-general.php?page=mail on update" method="post" id="mailonupdate-conf">
 			<table class="form-table">
@@ -318,8 +317,6 @@ class MailOnUpdate {
 			</table>
 			<p class="submit"><input type="submit" name="submit" value="<?php echo __('Save', 'mail-on-update'); ?>" /></p>
 		</form>
-		</p>
-		<p>
 			<table class="form-table">
 			<tr><th colspan="2" scope="row" valign="top"><?php echo __('Plugins to validate:', 'mail-on-update'); ?></th></tr>
 			<tr>
@@ -333,7 +330,6 @@ class MailOnUpdate {
 			</td>
 			</tr>
 			</table>
-		</p>
 	</div>
 	
 	<?php
